28c61cf57519011920063f3a8a06d9297a1ec52c
owlCode <akoladeo890@yahoo.com>
2023-05-11 14:03:01 +0100

    done with appointment section

--- src/components/component/Appointment.tsx
+++ src/components/component/Appointment.tsx
@@ -4,12 +4,12 @@ function Appointment() {
   return (
     <section className="appointment">
       <div className="appointment__box-1">
-        <h2>Book an appointment today to get your EV chargers installed correctly</h2>
-        <p>It takes just a few clicks to get an EV. installing a charger at your home should be just the same.</p>
-        <button className="bs__btn">Book Appointment<img src="/arrow-r.svg" alt="arrow right icon" /></button>
+        <h2 className="appointment__heading">Book an appointment today to get your EV chargers installed correctly</h2>
+        <p className="appointment__paragraph">It takes just a few clicks to get an EV. installing a charger at your home should be just the same.</p>
+        <button className="appointment__btn">Book Appointment<img src="/arrow-r.svg" alt="arrow right icon" /></button>
       </div>
       <div className="appointment__box-2">
-        <img src="/appointment-chargers.svg" alt="" />
+        <img className="appointment__img" src="/appointment-chargers.svg" alt="" />
       </div>
     </section>
   )
