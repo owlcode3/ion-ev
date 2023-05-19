e8b13407307822e2608cbffb031f35e97881de3d
owlCode <akoladeo890@yahoo.com>
2023-05-19 16:53:15 +0100

    done with categoryAndBrand ui responsiveness

--- src/components/component/CategoryAndBrand.tsx
+++ src/components/component/CategoryAndBrand.tsx
@@ -5,7 +5,9 @@ function CategoryAndBrand() {
     <div className="category-brand">
       <ul className="category-brand__lists">
         <li className="category-brand__list">
-          <span className="category-brand__cate-name">Categories:</span>
+          <span className="category-brand__cate-name">
+            Categories:<img className="category-brand__icon" src="/Arrow - Down 2.svg" alt="arrow down" />
+          </span>
           <ul className="category-brand__inner-lists">
             <li className="category-brand__inner-list">
               All products
@@ -29,10 +31,38 @@ function CategoryAndBrand() {
               Charger Installation
             </li>
           </ul>
+
+          <div className="category-brand__options">
+            <select className="category-brand__select" name="categories" id="categories">
+              <option className="category-brand__option" value="">
+              </option>
+              <option className="category-brand__option" value="All products">
+                All products
+              </option>
+              <option className="category-brand__option" value="Residential">
+                Residential
+              </option>
+              <option className="category-brand__option" value="Commercial">
+                Commercial
+              </option>
+              <option className="category-brand__option" value="EV Accessories">
+                EV Accessories
+              </option>
+              <option className="category-brand__option" value="EV Charging Cable">
+                EV Charging Cable
+              </option>
+              <option className="category-brand__option" value="Adapters">
+                Adapters
+              </option>
+              <option className="category-brand__option" value="Charger Installation">
+                Charger Installation
+              </option>
+            </select>
+          </div>
         </li>
 
         <li className="category-brand__list">
-          <span className="category-brand__cate-name">Car Brand:</span>
+          <span className="category-brand__cate-name">Car Brand:<img className="category-brand__icon" src="/Arrow - Down 2.svg" alt="arrow down" /></span>
           <ul className="category-brand__inner-lists">
             <li className="category-brand__inner-list">
               Audi
@@ -59,6 +89,37 @@ function CategoryAndBrand() {
               Ford
             </li>
           </ul>
+
+          <div className="category-brand__options">
+            <select className="category-brand__select" name="categories" id="categories">
+              <option className="category-brand__option" value="">
+              </option>
+              <option className="category-brand__option" value="Audi">
+                Audi
+              </option>
+              <option className="category-brand__option" value="BMW">
+                BMW
+              </option>
+              <option className="category-brand__option" value="Tesla">
+                Tesla
+              </option>
+              <option className="category-brand__option" value="Nissan">
+                Nissan
+              </option>
+              <option className="category-brand__option" value="KIA">
+                KIA
+              </option>
+              <option className="category-brand__option" value="Honda">
+                Honda
+              </option>
+              <option className="category-brand__option" value="Hyundai">
+                Hyundai
+              </option>
+              <option className="category-brand__option" value="Ford">
+                Ford
+              </option>
+            </select>
+          </div>
         </li>
       </ul>
     </div>
