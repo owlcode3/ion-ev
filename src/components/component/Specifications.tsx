59884cf1eb07b31fc787970971ae3c1dd9060b55
owlCode <akoladeo890@yahoo.com>
2023-07-03 15:49:11 +0100

    done with Specifications ui

---
+++ src/components/component/Specifications.tsx
@@ -0,0 +1,67 @@
+
+function Specifications() {
+  return (
+    <>
+      <ul className="pd-features__lists-3">
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Output Power:</span>
+          <div className="pd-features__list-3-inner">
+            <p>40A, 4.4 kW at 110 VAC; 9.6 kW at 240 VAC</p>
+          </div>
+        </li>
+
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Input Voltage:</span>
+          <div className="pd-features__list-3-inner">
+            <p>Single phase input power: 110 - 240 VAC ~ 60Hz</p>
+          </div>
+        </li>
+
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Input:</span>
+          <div className="pd-features__list-3-inner">
+            <p>2.5-foot input cable with NEMA 14-50 plug, NEMA 6-50 plug OR 2.5-foot hardwire pigtail</p>
+          </div>
+        </li>
+
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Output Cable:</span>
+          <div className="pd-features__list-3-inner">
+            <p>25-foot SAE J1772 standard plug</p>
+          </div>
+        </li>
+
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Dimensions:</span>
+          <div className="pd-features__list-3-inner">
+            <p>7.6” (173 mm) x 18.6”(474 mm) x 5.6” (141 mm)</p>
+          </div>
+        </li>
+
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Weight:</span>
+          <div className="pd-features__list-3-inner">
+            <p>15 lbs</p>
+          </div>
+        </li>
+
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Codes and Standards:</span>
+          <div className="pd-features__list-3-inner">
+            <p>FCC Part 15 Class B, NEC 625 compliant</p>
+          </div>
+        </li>
+
+        <li className="pd-features__list-3">
+          <span className="pd-features__list-3-text">Warranty:</span>
+          <div className="pd-features__list-3-inner">
+            <p>3-year warranty for use under normal residential operating conditions</p>
+          </div>
+        </li>
+
+      </ul>
+    </>
+  )
+}
+
+export default Specifications
