e2e905aacfba7962460b45ed38c15c9f259365d7
owlCode <akoladeo890@yahoo.com>
2023-07-29 16:05:13 +0100

    made some changes to congrats ui

--- src/components/component/Congrats.tsx
+++ src/components/component/Congrats.tsx
@@ -11,6 +11,8 @@ function Congrats() {
           <h2 className="congrats__heading">Congratulations!!!</h2>
           <p className="congrats__paragraph">Your order has been placed successfully! Check your mails for further information on shipping info and delivery updates.</p>
         </div>
+
+        <button className="congrats__btn" type="button">Ok</button>
       </div>
     </div>)
 }
